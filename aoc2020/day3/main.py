0fd79032e0616f0285c15648085b6b13a028cb72
Martin Keogh <martin1keogh@gmail.com>
2020-12-05 17:10:57 +0100

    feat: add day3 part1

---
+++ aoc2020/day3/main.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass
+from typing import List, Literal, Callable
+
+from pydantic import BaseModel, root_validator
+
+from aoc2020.shared.puzzle import Puzzle, PuzzleDownloader
+from aoc2020.shared.solver import Solver
+
+
+@dataclass
+class Slope:
+    down: int
+    right: int
+
+
+ForestSpot = Literal[".", "#"]
+
+
+class Forest(BaseModel):
+    __root__: List[List[ForestSpot]]
+
+    @root_validator(pre=True)
+    def _ensure_constant_width(cls, values):
+        widths = map(len, values["__root__"])
+        if len(set(widths)) != 1:
+            raise ValueError("Non constant width found in forest.")
+        return values
+
+    @property
+    def height(self) -> int:
+        return len(self.__root__)
+
+    @property
+    def width(self) -> int:
+        if not self.__root__:
+            return 0
+        return len(self.__root__[0])
+
+    def path_to_end(self, slope: Slope) -> List[ForestSpot]:
+        x, y = 0, 0
+        visited_spots: List[ForestSpot] = []
+        while y < self.height:
+            visited_spots.append(self.__root__[y][x])
+            y += slope.down
+            x += slope.right
+            x %= self.width
+        return visited_spots
+
+
+@dataclass
+class SolverDay3(Solver):
+    puzzle: Puzzle[Forest]
+
+    def part1(self) -> int:
+        path = self.puzzle.data.path_to_end(Slope(down=1, right=3))
+        return path.count("#")
+
+    @staticmethod
+    def parser(string: str) -> Forest:
+        by_line = string.splitlines()
+        # using `list` directly in the map() makes mypy sad
+        list_: Callable[[str], List[str]] = list
+        by_character = map(list_, by_line)
+        return Forest(__root__=list(by_character))
+
+
+if __name__ == '__main__':
+    SolverDay3(PuzzleDownloader(day=3, parser=SolverDay3.parser).get_puzzle()).run()
---
+++ tests/day3/test_main.py
@@ -0,0 +1,28 @@
+from typing import List, Callable
+
+from aoc2020.day3.main import SolverDay3, Forest
+from tests.utils.puzzle_examples_checker import PuzzleExamplesChecker, Example
+
+
+class TestSolverDay3(PuzzleExamplesChecker):
+    day: int = 3
+    solver: SolverDay3 = SolverDay3
+    examples: List[Example] = [
+        Example(
+            data="""\
+                ..##.......
+                #...#...#..
+                .#....#..#.
+                ..#.#...#.#
+                .#...##..#.
+                ..#.##.....
+                .#.#.#....#
+                .#........#
+                #.##...#...
+                #...##....#
+                .#..#...#.#""",
+            solution_part1=7,
+        )
+    ]
+    # hack to get around the fact that python sucks ass
+    parser: Callable[[str], Forest] = lambda x, y: SolverDay3.parser(y)
