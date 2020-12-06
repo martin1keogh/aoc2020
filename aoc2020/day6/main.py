1b61b88ad1859f7e235be4554dfe3c4c17ac13ed
Martin Keogh <martin1keogh@gmail.com>
2020-12-06 14:18:05 +0100

    feat: add day6 part2

--- aoc2020/day6/main.py
+++ aoc2020/day6/main.py
@@ -1,26 +1,21 @@
+from functools import reduce
 from typing import List
-
-from pydantic import BaseModel, Field
 
 from aoc2020.shared.parser_utils import groupwise_parser
 from aoc2020.shared.puzzle import Puzzle, PuzzleDownloader
 from aoc2020.shared.solver import Solver
 
 
-class Answers(BaseModel):
-    __root__: str = Field(..., regex="^[a-zA-Z]+$")
-
-
 class SolverDay6(Solver):
-    puzzle: Puzzle[List[List[Answers]]]
+    puzzle: Puzzle[List[List[str]]]
 
     @staticmethod
     @groupwise_parser
-    def parser(group: List[str]) -> List[Answers]:
+    def parser(group: List[str]) -> List[str]:
         answers = []
         for answer_per_person in group:
             no_blanks = answer_per_person.replace(" ", "")
-            answers.append(Answers(__root__=no_blanks))
+            answers.append(no_blanks)
         return answers
 
     def part1(self) -> int:
@@ -28,8 +23,16 @@ class SolverDay6(Solver):
         for answers_per_group in self.puzzle.data:
             answers_in_group = set()
             for answers_per_person in answers_per_group:
-                answers_in_group.update(list(answers_per_person.__root__))
+                answers_in_group.update(list(answers_per_person))
             count += len(answers_in_group)
+        return count
+
+    def part2(self) -> int:
+        count = 0
+        for answers_per_group in self.puzzle.data:
+            as_sets = map(set, answers_per_group)
+            common_answers = reduce(set.intersection, as_sets)
+            count += len(common_answers)
         return count
 
 
--- tests/day6/test_main.py
+++ tests/day6/test_main.py
@@ -1,6 +1,6 @@
 from typing import List, Callable
 
-from aoc2020.day6.main import SolverDay6, Answer
+from aoc2020.day6.main import SolverDay6
 from tests.utils.puzzle_examples_checker import PuzzleExamplesChecker, Example
 
 
@@ -25,6 +25,8 @@ class TestSolverDay6(PuzzleExamplesChecker):
                 a
 
                 b""",
-            solution_part1=11),
+            solution_part1=11,
+            solution_part2=6,
+        ),
     ]
-    parser: Callable[[str], List[Answer]] = SolverDay6.parser
+    parser: Callable[[str], List[str]] = SolverDay6.parser
