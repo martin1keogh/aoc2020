9f465ea11ec90ce029fd0fcf9b16ce4b66edc461
Martin Keogh <martin1keogh@gmail.com>
2020-12-08 10:35:52 +0100

    refacto: prepare day8 code for part2

--- aoc2020/day8/main.py
+++ aoc2020/day8/main.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
 import re
+from dataclasses import dataclass, field
 from typing import List, Literal, ClassVar, Set
 
 from pydantic import BaseModel
 
+from aoc2020.shared.models import NoResultFoundException
 from aoc2020.shared.parser_utils import linewise_parser
 from aoc2020.shared.puzzle import Puzzle, PuzzleDownloader
 from aoc2020.shared.solver import Solver
@@ -16,6 +18,40 @@ class Instruction(BaseModel):
     arg1: int
 
     regex: ClassVar[str] = "(?P<code>\\w{3}) (?P<arg1>(\+|-)\\d+)"
+
+
+class LoopDetected(RuntimeError):
+    def __init__(self, acc: int):
+        self.final_accumulator_value = acc
+
+
+@dataclass
+class Interpreter:
+    instructions: List[Instruction]
+    _accumulator: int = field(init=False, default=0)
+    _current_index: int = field(init=False, default=0)
+    _visited_indices: Set[int] = field(init=False, default_factory=set)
+
+    def run(self) -> int:
+        while True:
+            if self._current_index in self._visited_indices:
+                raise LoopDetected(self._accumulator)
+            if self._current_index >= len(self.instructions):
+                return self._accumulator
+            self.run_step()
+
+    def run_step(self) -> None:
+        self._visited_indices.add(self._current_index)
+        current_instruction = self.instructions[self._current_index]
+        if current_instruction.code == "acc":
+            self._accumulator += current_instruction.arg1
+            self._current_index += 1
+        elif current_instruction.code == "nop":
+            self._current_index += 1
+        elif current_instruction.code == "jmp":
+            self._current_index += current_instruction.arg1
+        else:
+            assert_never(current_instruction.code)
 
 
 class SolverDay8(Solver):
@@ -30,22 +66,12 @@ class SolverDay8(Solver):
             raise ValueError(f"Unknown instruction {line}")
 
     def part1(self) -> int:
-        accumulator = 0
-        current_index = 0
-        visited_indices: Set[int] = set()
-        while current_index not in visited_indices:
-            visited_indices.add(current_index)
-            current_instruction = self.puzzle.data[current_index]
-            if current_instruction.code == "acc":
-                accumulator += current_instruction.arg1
-                current_index += 1
-            elif current_instruction.code == "nop":
-                current_index += 1
-            elif current_instruction.code == "jmp":
-                current_index += current_instruction.arg1
-            else:
-                assert_never(current_instruction.code)
-        return accumulator
+        interpreter = Interpreter(self.puzzle.data)
+        try:
+            interpreter.run()
+            raise NoResultFoundException
+        except LoopDetected as ld:
+            return ld.final_accumulator_value
 
 
 if __name__ == '__main__':
