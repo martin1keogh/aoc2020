cf4d193c423b0222a34ad2e68b7dd48eaecffcd1
Martin Keogh <martin1keogh@gmail.com>
2020-12-13 17:00:23 +0100

    refacto: replace some map() with list-comprehensions

--- aoc2020/day13/main.py
+++ aoc2020/day13/main.py
@@ -16,10 +16,6 @@ class Notes:
 class SolverDay13(Solver):
     puzzle: Puzzle[Notes]
 
-    @property
-    def arrival_time(self) -> int:
-        return self.puzzle.data.arrival_time
-
     @staticmethod
     def parser(input_: str) -> Notes:
         line1, line2 = input_.splitlines()
@@ -27,17 +23,18 @@ class SolverDay13(Solver):
         return Notes(arrival_time=int(line1), buses=buses)
 
     def part1(self) -> int:
-        with_times_to_wait = [(bus, -self.arrival_time % bus) for _, bus in self.puzzle.data.buses]
+        with_times_to_wait = [(bus, -self.puzzle.data.arrival_time % bus) for _, bus in self.puzzle.data.buses]
         bus, ttw = min(with_times_to_wait, key=lambda wttw: wttw[1])
         return bus * ttw
 
     def part2(self) -> int:
         # check all the frequencies are coprimes
-        assert reduce(math.gcd, map(lambda x: x[0], self.puzzle.data.buses)) == 1
+        assert reduce(math.gcd, (freq for _, freq in self.puzzle.data.buses)) == 1
 
         # chinese remainder algorithm
         result = 0
-        m = math.prod(map(lambda x: x[1], self.puzzle.data.buses))
+        m = math.prod(freq for _, freq in self.puzzle.data.buses)
+
         for remainder, freq in self.puzzle.data.buses:
             b = m // freq
             result += - remainder * b * pow(b, -1, freq)
